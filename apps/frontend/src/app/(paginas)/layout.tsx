c1e76f19a2e5dee8dd5a897fb6f571d40595d491
gilbertodfl <dilbert.gil@gmail.com>
2025-05-29 22:14:08 -0300

    feat: criação FormEvento

--- apps/frontend/src/app/(paginas)/layout.tsx
+++ apps/frontend/src/app/(paginas)/layout.tsx
@@ -1,5 +1,13 @@
+"use client";
 import Pagina from "@/components/template/Pagina";
+import { ProvedorContextoEvento } from "@/data/contexts/ContextoEvento";
 
 export default function Layout(props: any) {
-  return <Pagina>{props.children}</Pagina>;
+  return (
+    <Pagina>
+      <ProvedorContextoEvento>
+        {props.children}
+      </ProvedorContextoEvento>
+    </Pagina>
+  );
 }
